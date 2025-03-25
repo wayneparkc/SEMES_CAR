abdd5bb50f5ebe980aea5e4aa53f125cddb29e43
박창빈 <changbin10528@gmail.com>
2025-03-25 14:31:49 +0900

    Console 클래스 생성

--- SEMES_CAR/Car.cpp
+++ SEMES_CAR/Car.cpp
@@ -4,6 +4,7 @@
 #include "part.h"
 
 #define interface struct
+#define CLEAR_SCREEN "\033[H\033[2J"
 
 class different_brand_error : public std::exception {
 public:
@@ -25,6 +26,18 @@ interface Car{
 	Engine engine; 
 	BreakSystem bs;
 	SteeringSystem steer;
+
+	void setEngine(Engine _engine) {
+		this->engine = _engine;
+	}
+
+	void setBreak(BreakSystem _bs) {
+		this->bs = _bs;
+	}
+
+	void setSteering(SteeringSystem _steer) {
+		this->steer = _steer;
+	}
 
 	void checkSteer() {
 		if (BreakSystem::BOSCH_B == bs && SteeringSystem::BOSCH_S != steer) {
@@ -92,10 +105,120 @@ public:
 		checkBreakSystem();
 	}
 };
-std::unique_ptr<Car> Factor(std::string name) {
-	if(name == "Sedan")
+std::unique_ptr<Car> Factor(int type) {
+	if(type == 1)
 		return std::make_unique<Sedan>();
-	else if (name == "SUV")
+	else if (type == 2)
 		return std::make_unique<SUV>();
-	return std::make_unique<Truck>();
+	else if(type == 3)
+		return std::make_unique<Truck>();
+	return std::make_unique<Car>();
+}
+
+class Console {
+	int step = 1;
+
+public:
+	int get_step() {
+		return step;
+	}
+
+	int input() {
+		printf("INPUT > ");
+		int num;
+		std::cin >> num;
+		return num;
+	}
+
+	int choose_type() {
+		printf(CLEAR_SCREEN);
+		printf("        ______________\n");
+		printf("       /|            | \n");
+		printf("  ____/_|_____________|____\n");
+		printf(" |                      O  |\n");
+		printf(" '-(@)----------------(@)--'\n");
+		printf("===============================\n");
+		printf("어떤 차량 타입을 선택할까요?\n");
+		printf("1. Sedan\n");
+		printf("2. SUV\n");
+		printf("3. Truck\n");
+		printf("4. 종료\n");
+		printf("===============================\n");
+		return input();
+	}
+
+	int choose_engine() {
+		printf(CLEAR_SCREEN);
+		printf("어떤 엔진을 탑재할까요?\n");
+		printf("0. 뒤로가기\n");
+		printf("1. GM\n");
+		printf("2. TOYOTA\n");
+		printf("3. WIA\n");
+		printf("4. 고장난 엔진\n");
+		printf("===============================\n");
+		return input();
+	}
+
+	int choose_break() {
+		printf(CLEAR_SCREEN);
+		printf("어떤 제동장치를 선택할까요?\n");
+		printf("0. 뒤로가기\n");
+		printf("1. MANDO\n");
+		printf("2. CONTINENTAL\n");
+		printf("3. BOSCH\n");
+		printf("===============================\n");
+		return input();
+	}
+
+	int choose_steering() {
+		printf(CLEAR_SCREEN);
+		printf("어떤 조향장치를 선택할까요?\n");
+		printf("0. 뒤로가기\n");
+		printf("1. BOSCH\n");
+		printf("2. MOBIS\n");
+		printf("===============================\n");
+		return input();
+	}
+
+	int choose_action() {
+		printf(CLEAR_SCREEN);
+		printf("멋진 차량이 완성되었습니다.\n");
+		printf("어떤 동작을 할까요?\n");
+		printf("0. 처음 화면으로 돌아가기\n");
+		printf("1. RUN\n");
+		printf("2. Info\n");
+		printf("3. Test\n");
+		printf("===============================\n");
+		return input();
+	}
+
+};
+
+int main() {
+
+	std::unique_ptr<Car> car;
+	Console console;
+
+	while (1) {
+
+		if (console.get_step() == 1) {
+			int type = console.choose_type();
+			car = Factor(type);
+			// 차량 타입이 1, 2, 3이 아니면 종료
+			if (!(type>=1 && type<=3)) break;
+		}
+		else if (console.get_step() == 2) {
+			int engine = console.choose_engine();
+			
+		}
+		else if (console.get_step() == 3) {
+
+		}
+		else if (console.get_step() == 4) {
+
+		}
+		else if (console.get_step() == 5) {
+
+		}
+	}
 }
