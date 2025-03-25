3e6ee57100f428c2f31b9ed465c28f3b4590e6ec
박창빈 <changbin10528@gmail.com>
2025-03-25 13:51:31 +0900

    ENUM을 숫자로

--- SEMES_CAR/Car.cpp
+++ SEMES_CAR/Car.cpp
@@ -2,11 +2,22 @@
 #include <string>
 #include <unordered_map>
 #include "part.h"
+
 #define interface struct
 
 class different_brand_error : public std::exception {
 public:
 	different_brand_error(const char* msg) : exception{ msg } {};
+};
+
+class not_make_break : public std::exception {
+public:
+	not_make_break(const char* msg) : exception{ msg } {};
+};
+
+class not_make_engine : public std::exception {
+public:
+	not_make_engine(const char* msg) : exception{ msg } {};
 };
 
 interface Car{
@@ -29,7 +40,7 @@ private:
 	void checkBreak() {
 		if (BreakSystem::CONTINENTAL == bs) {
 			// 에러 발생
-
+			throw not_make_break("CONTINENTAL 사는 세단의 제동장치를 만들지 않습니다.");
 		}
 	}
 
@@ -46,7 +57,7 @@ private:
 	void checkEngine() {
 		if (Engine::TOYOTA == engine) {
 			// 에러 발생
-
+			throw not_make_engine("도요타는 SUV용 엔진을 만들지 않습니다.");
 		}
 	}
 
@@ -63,13 +74,13 @@ private:
 	void checkEngine() {
 		if (Engine::WIA == engine) {
 			// 에러 발생
-
+			throw not_make_engine("WIA는 Truck용 엔진을 만들지 않습니다.");
 		}
 	}
 	void checkBreakSystem() {
 		if (BreakSystem::MANDO == bs) {
 			// 에러 발생
-
+			throw not_make_break(" Mando는 Truck용 제동장치를 만들지 않습니다.");
 		}
 	}
 
