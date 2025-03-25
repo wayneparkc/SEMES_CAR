cb53aacb6e97e44575c1b450d7766870b54c8b18
Lee Jin Ki <dlwlsrl99@gmail.com>
2025-03-25 11:43:37 +0900

    [merge] Car.cpp , Test() 점검 merge.

--- SEMES_CAR/Car.cpp
+++ SEMES_CAR/Car.cpp
@@ -2,30 +2,84 @@
 #include <string>
 #include <unordered_map>
 #include "part.h"
+#define interface struct
 
-class Car {
+class different_brand_error : public std::exception {
 public:
-	virtual void Test() = 0;
+	different_brand_error(const char* msg) : exception{ msg } {};
+};
+
+interface Car{
+	Engine engine;
+	BreakSystem bs;
+	SteeringSystem steer;
+
+	void checkSteer() {
+		if (BreakSystem::BOSCH_B == bs && SteeringSystem::BOSCH_S != steer) {
+			// 에러 발생
+			throw different_brand_error("BOSCH 제동장치는 BOSCH 조향장치와 호환됩니다.");
+		}
+	}
+	virtual void test() = 0;
 };
 
 class Sedan : public Car {
-	void Test() override {
+private:
+	void checkBreak() {
+		if (BreakSystem::CONTINENTAL == bs) {
+			// 에러 발생
 
+		}
+	}
+
+public:
+	// Car을(를) 통해 상속됨
+	void test() override {
+		checkSteer();
+		checkBreak();
 	}
 };
 
 class SUV : public Car {
-	void Test() override {
+private:
+	void checkEngine() {
+		if (Engine::TOYOTA == engine) {
+			// 에러 발생
 
+		}
+	}
+
+public:
+	// Car을(를) 통해 상속됨
+	void test() override {
+		checkSteer();
+		checkEngine();
 	}
 };
 
 class Truck : public Car {
-	void Test() override {
+private:
+	void checkEngine() {
+		if (Engine::WIA == engine) {
+			// 에러 발생
 
+		}
+	}
+	void checkBreakSystem() {
+		if (BreakSystem::MANDO == bs) {
+			// 에러 발생
+
+		}
+	}
+
+public:
+	// Car을(를) 통해 상속됨
+	void test() override {
+		checkSteer();
+		checkEngine();
+		checkBreakSystem();
 	}
 };
-
 std::shared_ptr<Car> Factor(std::string name) {
 	if(name == "Sedan")
 		return std::make_shared<Sedan>();
