65cf2ed50223a9e8da0618ae62ed42696d7ad1e0
박창빈 <changbin10528@gmail.com>
2025-03-25 16:10:15 +0900

    step 증감 추가

--- SEMES_CAR/Car.cpp
+++ SEMES_CAR/Car.cpp
@@ -45,21 +45,23 @@ interface Car{
 	}
 
 	std::string getEngine() {
-		if (static_cast<int>(this->engine) == 1) return "GM";
-		else if (static_cast<int>(this->engine) == 2) return "TOYOTA";
-		else if (static_cast<int>(this->engine) == 3) return "WIA";
+		if (this->engine == Engine::GM) return "GM";
+		else if (this->engine == Engine::TOYOTA) return "TOYOTA";
+		else if (this->engine == Engine::WIA) return "WIA";
 		return "";
 	}
 
 	std::string getBreak() {
-		if (static_cast<int>(this->bs) == 1) return "MANDO";
-		else if (static_cast<int>(this->bs) == 2) return "CONTINENTAL";
-		else if (static_cast<int>(this->bs) == 3) return "BOSCH_B";
+		if (this->bs == BreakSystem::MANDO) return "MANDO";
+		else if (this->bs == BreakSystem::CONTINENTAL) return "CONTINENTAL";
+		else if (this->bs == BreakSystem::BOSCH_B) return "BOSCH_B";
+		return "";
 	}
 
 	std::string getSteering() {
-		if (static_cast<int>(this->steer) == 1) return "BOSCH_S";
-		else if (static_cast<int>(this->steer) == 2) return "MOBIS";
+		if (this->steer == SteeringSystem::BOSCH_S) return "BOSCH_S";
+		else if (this->steer == SteeringSystem::MOBIS) return "MOBIS";
+		return "";
 	}
 
 	void checkSteer() {
@@ -76,6 +78,9 @@ interface Car{
 		else {
 			std::cout << "엔진이 고장났습니다.\n";
 		}
+		printf("동작 정지 [Enter]");
+		std::cin.ignore();
+		std::cin.get();
 	}
 
 	virtual void test() = 0;
@@ -145,13 +150,26 @@ std::unique_ptr<Car> Factor(int type) {
 		return std::make_unique<SUV>();
 	else if(type == 3)
 		return std::make_unique<Truck>();
-	return std::make_unique<Car>();
+	return nullptr;
 }
 
 class Console {
 	int step = 1;
 
 public:
+
+	void set_step(int input) {
+		if (input == 0 && this->step == 5) {
+			this->step = 1;
+		}
+		else if (input == 0 && this->step >= 1) {
+			this->step -= 1;
+		}
+		else if (this->step <= 4) {
+			this->step += 1;
+		}
+	}
+
 	int get_step() {
 		return step;
 	}
@@ -177,7 +195,9 @@ public:
 		printf("3. Truck\n");
 		printf("4. 종료\n");
 		printf("===============================\n");
-		return input();
+		int val = input();
+		set_step(val);
+		return val;
 	}
 
 	int choose_engine() {
@@ -189,7 +209,9 @@ public:
 		printf("3. WIA\n");
 		printf("4. 고장난 엔진\n");
 		printf("===============================\n");
-		return input();
+		int val = input();
+		set_step(val);
+		return val;
 	}
 
 	int choose_break() {
@@ -200,7 +222,9 @@ public:
 		printf("2. CONTINENTAL\n");
 		printf("3. BOSCH\n");
 		printf("===============================\n");
-		return input();
+		int val = input();
+		set_step(val);
+		return val;
 	}
 
 	int choose_steering() {
@@ -210,7 +234,9 @@ public:
 		printf("1. BOSCH\n");
 		printf("2. MOBIS\n");
 		printf("===============================\n");
-		return input();
+		int val = input();
+		set_step(val);
+		return val;
 	}
 
 	int choose_action() {
@@ -222,7 +248,9 @@ public:
 		printf("2. Info\n");
 		printf("3. Test\n");
 		printf("===============================\n");
-		return input();
+		int val = input();
+		set_step(val);
+		return val;
 	}
 
 	void info(Car& car) {
@@ -261,7 +289,9 @@ public:
 		std::cout << "제동 장치 : " << car.getBreak() << "\n";
 		std::cout << "조향 장치 : " << car.getSteering() << "\n";
 		printf("===============================\n");
-		printf("정보 닫기 ");
+		printf("정보 닫기 [Enter]");
+		std::cin.ignore();
+		std::cin.get();
 	}
 
 };
